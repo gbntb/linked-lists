8c86aa975d078a43b8d80d8a5434c575d73be881
gbntb <228612152+gbntb@users.noreply.github.com>
2026-01-06 13:32:29 -0400

    Implement #size method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -28,4 +28,16 @@ class LinkedList
     self.tail = node if head.nil?
     self.head = node
   end
+
+  def size
+    counter = 0
+    current_node = head
+
+    until current_node.nil?
+      counter += 1
+      current_node = current_node.next_node
+    end
+
+    counter
+  end
 end
